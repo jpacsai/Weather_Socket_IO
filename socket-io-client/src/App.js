53e6968a3f864afc57508c7c9e29f4779b8abded
jpacsai <judit.pacsai@gmail.com>
2019-07-18 17:56:29 +0200

    add client

---
+++ socket-io-client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import socketIOClient from "socket.io-client";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        App
+      </div>
+    );
+  }
+}
+
+export default App;
